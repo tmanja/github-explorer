909b37b201df281dd11e8342e507b139812df13d
Thalles Manjaterra <thalles.devraiz@gmail.com>
2022-04-02 11:51:46 -0300

    rendering a react component into the DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
-createRoot(document.getElementById("root"))
-.render(<h1>Hello Cruel World</h1>);
+createRoot(document.getElementById("root")).render(<App />);
